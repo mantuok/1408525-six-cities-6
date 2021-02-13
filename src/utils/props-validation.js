f37c59ef3dc756a9172f36383a4943abc5046b46
mantuok <mantuok@gmail.com>
2021-02-14 01:02:59 +0300

    Рефакторинг props

--- src/utils/props-validation.js
+++ src/utils/props-validation.js
@@ -39,51 +39,6 @@ export const offerPropTypes = PropTypes.shape({
   type: PropTypes.string.isRequired
 });
 
-export const offersPropTypes = PropTypes.arrayOf(PropTypes.shape({
-  bedrooms: PropTypes.number.isRequired,
-  city: PropTypes.shape({
-    location: PropTypes.shape({
-      latitude: PropTypes.number.isRequired,
-      longitude: PropTypes.number.isRequired,
-      zoom: PropTypes.number.isRequired
-    }),
-    name: PropTypes.string.isRequired
-  }),
-  description: PropTypes.string.isRequired,
-  goods: PropTypes.arrayOf(
-      PropTypes.string.isRequired
-  ),
-  host: PropTypes.shape({
-    avatarUrl: PropTypes.string.isRequired,
-    id: PropTypes.number.isRequired,
-    isPro: PropTypes.bool.isRequired,
-    name: PropTypes.string.isRequired
-  }),
-  id: PropTypes.number.isRequired,
-  images: PropTypes.arrayOf(
-      PropTypes.string.isRequired
-  ),
-  isFavorite: PropTypes.bool.isRequired,
-  isPremium: PropTypes.bool.isRequired,
-  location: PropTypes.shape({
-    latitude: PropTypes.number.isRequired,
-    longitude: PropTypes.number.isRequired,
-    zoom: PropTypes.number.isRequired
-  }),
-  maxAdults: PropTypes.number.isRequired,
-  previewImage: PropTypes.string.isRequired,
-  price: PropTypes.number.isRequired,
-  rating: PropTypes.number.isRequired,
-  title: PropTypes.string.isRequired,
-  type: PropTypes.string.isRequired
-}));
-
-export const goodPropTypes = PropTypes.string.isRequired;
-
-export const imagePropTypes = PropTypes.string.isRequired;
-
-export const titlePropTypes = PropTypes.string.isRequired;
-
 export const reviewPropTypes = PropTypes.shape({
   comment: PropTypes.string.isRequired,
   date: PropTypes.string.isRequired,
@@ -97,17 +52,10 @@ export const reviewPropTypes = PropTypes.shape({
   })
 });
 
-export const reviewsPropTypes = PropTypes.arrayOf(PropTypes.shape({
-  comment: PropTypes.string.isRequired,
-  date: PropTypes.string.isRequired,
-  id: PropTypes.number.isRequired,
-  rating: PropTypes.number.isRequired,
-  user: PropTypes.shape({
-    avatarUrl: PropTypes.string.isRequired,
-    id: PropTypes.number.isRequired,
-    isPro: PropTypes.bool.isRequired,
-    name: PropTypes.string.isRequired
-  })
-}));
+export const offersPropTypes = PropTypes.arrayOf(offerPropTypes);
+export const reviewsPropTypes = PropTypes.arrayOf(reviewPropTypes);
 
+export const goodPropTypes = PropTypes.string.isRequired;
+export const imagePropTypes = PropTypes.string.isRequired;
+export const titlePropTypes = PropTypes.string.isRequired;
 export const functionPropTypes = PropTypes.func.isRequired;
