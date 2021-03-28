bcd05c9dcae3bc2fa303da189e0800cb698de7a1
mantuok <mantuok@gmail.com>
2021-03-28 23:02:44 +0300

    Завершены тесты на асинхронные операции

--- src/store/user/user.test.js
+++ src/store/user/user.test.js
@@ -1,17 +1,8 @@
-import MockAdapter from 'axios-mock-adapter';
-import {createApi} from '../../services/api';
 import {user} from './user';
 import {ActionType} from '../action';
 import {
-  checkAuth,
-  login,
-  logout
-} from '../api-actions';
-import {
   AuthorizationStatus
 } from '../../const';
-
-const api = createApi(() => {});
 
 describe(`Reducers work correctly`, () => {
   it(`Reducer without additional parameters should return initial state`, () => {
@@ -52,78 +43,4 @@ describe(`Reducers work correctly`, () => {
     expect(user(state, setUserData))
       .toEqual({userEmail: `johndoe@gmail.com`, userAvatar: `img/1.png`})
   });
-});
-
-describe(`Async operations work correctly`, () => {
-  it(`Should make a correct GET call to API login`, () => {
-    const apiMock = new MockAdapter(api);
-    const dispatch = jest.fn();
-    const checkAuthLoader = checkAuth();
-
-    apiMock
-      .onGet(`/login`)
-      .reply(200, [{fake: true}]);
-
-    return checkAuthLoader(dispatch, () => {}, api)
-    .then(() => {
-      expect(dispatch).toHaveBeenCalledTimes(1);
-      expect(dispatch).toHaveBeenCalledWith({
-        type: ActionType.REQUIRED_AUTHORIZATION,
-        payload: AuthorizationStatus.AUTH
-      });
-    });
-  });
-
-  it(`Should make a correct POST call to API login`, () => {
-    const apiMock = new MockAdapter(api);
-    const dispatch = jest.fn();
-    const fakeUser = {email: `johndoe@gmail.com`, password: `12345`};
-    const loginLoader = login(fakeUser);
-
-    apiMock
-      .onPost(`/login`)
-      .reply(200, [{fake: true}]);
-
-    return loginLoader(dispatch, () => {}, api)
-    .then(() => {
-      expect(dispatch).toHaveBeenCalledTimes(2);
-
-      expect(dispatch).toHaveBeenNthCalledWith(1, {
-        type: ActionType.SET_USER_DATA,
-        payload: [{fake: true}]
-      });
-
-      expect(dispatch).toHaveBeenNthCalledWith(2, {
-        type: ActionType.REQUIRED_AUTHORIZATION,
-        payload: AuthorizationStatus.AUTH
-      });
-    });
-  });
-
-  it(`Should make a correct GET call to API logout`, () => {
-    const apiMock = new MockAdapter(api);
-    const dispatch = jest.fn();
-    const logoutLoader = logout();
-
-    apiMock
-      .onGet(`/logout`)
-      .reply(200, [{fake: true}]);
-
-    return logoutLoader(dispatch, () => {}, api)
-    .then(() => {
-      expect(dispatch).toHaveBeenCalledTimes(2);
-
-      expect(dispatch).toHaveBeenNthCalledWith(1, {
-        type: ActionType.SET_USER_DATA,
-        payload: {}
-      });
-
-      expect(dispatch).toHaveBeenNthCalledWith(2, {
-        type: ActionType.REQUIRED_AUTHORIZATION,
-        payload: AuthorizationStatus.NO_AUTH
-      });
-    });
-  });
-
-
 });
